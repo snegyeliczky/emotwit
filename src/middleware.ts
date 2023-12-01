6e406fc3415ef7adf093ce5280fe7ef26f103e53
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-12-01 16:11:24 +0100

    add: style

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import {authMiddleware} from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   afterAuth(auth, req, evt) {
-    console.log("user", auth.userId);
+    console.log("Middleware data:", { auth, req, evt });
   },
 });
 
