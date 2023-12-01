f11e59584c3f811460c68c935e31b7693db26eb8
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-12-01 12:14:23 +0100

    add: planetscale connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 import { api } from "~/utils/api";
 
+
 export default function Home() {
+
+
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
--- src/server/db.ts
+++ src/server/db.ts
@@ -14,3 +14,20 @@ export const db =
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+
+// const prisma = new PrismaClient()
+//
+// async function main() {
+//   // ... you will write your Prisma Client queries here
+// }
+//
+// main()
+//     .then(async () => {
+//       await prisma.$disconnect()
+//     })
+//     .catch(async (e) => {
+//       console.error(e)
+//       await prisma.$disconnect()
+//       process.exit(1)
+//     })
